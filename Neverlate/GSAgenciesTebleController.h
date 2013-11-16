fe21cdfdb1617f82d0f2ec6281a89972e139b275
Endika Gutiérrez Salas <endika@Endikas-MacBook-Pro.local>
2013-11-16 21:03:47 +0100

    CocoaPods added. ECSlidingViewController included.

---
+++ Neverlate/GSAgenciesTebleController.h
@@ -0,0 +1,13 @@
+//
+//  GSAgenciesTebleController.h
+//  Neverlate
+//
+//  Created by Endika Gutiérrez Salas on 16/11/13.
+//  Copyright (c) 2013 Endika Gutiérrez Salas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSAgenciesTebleController : UITableViewController
+
+@end
